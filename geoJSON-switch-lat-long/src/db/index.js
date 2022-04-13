0b193107e4154da1f50deb41dbfa917e45ccb99d
maximeesilv <maxime.gasnier.1@gmail.com>
2022-04-14 01:29:36 +0200

    :ambulance: fix super bug
    Ouhlalalahhhh bug de fou

--- geoJSON-switch-lat-long/src/db/index.js
+++ geoJSON-switch-lat-long/src/db/index.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 
 export async function connectToDb() {
-    mongoose.connect(process.env.MONGO_APP + process.env.MONGO_PWD + process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_APP + process.env.MONGO_PWD + process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
       }, (err, client) => {
         if(err) console.log(err)
         if(client) console.log('Connection Established with Database, Ready to play')
     });
+
+    return mongoose
 }
--- geoJSON-switch-lat-long/src/index.js
+++ geoJSON-switch-lat-long/src/index.js
@@ -4,4 +4,10 @@ import { connectToDb } from './db';
 
 console.log(figlet.textSync('Checkit - GeoJSON Switch'));
 
-await connectToDb();
+const db = await connectToDb();
+db.collection('events');
+
+console.log(figlet.textSync('All events down here'))
+
+const elements = await events.find();
+console.log(elements);
